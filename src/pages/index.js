78246891c149f57898042b1c74c35306b95635a1
Angel Villalobos <angel.villalobos@pixel506.com>
2021-12-06 15:52:42 -0600

    Test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,14 +8,15 @@ import Hero from "../modules/Hero"
 import TwoColumnsLeftListAndImage from "../modules/TwoColumnsLeftListAndImage"
 
 const IndexPage = ({ serverData }) => {
-  const {
-    zones: { MainContentZone },
-  } = serverData
+  // const {
+  //   zones: { MainContentZone },
+  // } = serverData
 
   return (
     <Layout>
-      <p>Texto</p>
-      {MainContentZone && <Hero item={MainContentZone[0]} />}
+      <h3>Texto</h3>
+      <p>Prueba de page</p>
+      {/* {MainContentZone && <Hero item={MainContentZone[0]} />}
       {MainContentZone && <ContentAndImage item={MainContentZone[1]} />}
       {MainContentZone && (
         <TwoColumnsLeftListAndImage item={MainContentZone[2]} />
@@ -27,24 +28,24 @@ const IndexPage = ({ serverData }) => {
       {MainContentZone && <ContentAndImage item={MainContentZone[5]} />}
       {MainContentZone && <ClientsAndPartners item={MainContentZone[6]} />}
       {MainContentZone && <ContentAndImage item={MainContentZone[7]} />}
-      {MainContentZone && <CTAFooter item={MainContentZone[8]} />}
+      {MainContentZone && <CTAFooter item={MainContentZone[8]} />} */}
     </Layout>
   )
 }
 
-export async function getServerData() {
-  const rest = await fetch(
-    "https://api.aglty.io/800c8044/fetch/en-us/page/111",
-    {
-      headers: {
-        APIKEY:
-          "defaultlive.42fb0e27ae9606d57c06565f2369f5de856c3982facea37d12308ff2d453851f",
-      },
-    }
-  )
-  return {
-    props: await rest.json(),
-  }
-}
+// export async function getServerData() {
+//   const rest = await fetch(
+//     "https://api.aglty.io/800c8044/fetch/en-us/page/111",
+//     {
+//       headers: {
+//         APIKEY:
+//           "defaultlive.42fb0e27ae9606d57c06565f2369f5de856c3982facea37d12308ff2d453851f",
+//       },
+//     }
+//   )
+//   return {
+//     props: await rest.json(),
+//   }
+// }
 
 export default IndexPage
